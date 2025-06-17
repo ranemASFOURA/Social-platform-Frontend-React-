b36b31efd8a461933774e557da893bdc7c59b22d
ranem <ranem.y.asfoura@gmail.com>
2025-06-17 22:45:30 +0300

    improvement and add login

--- src/App.js
+++ src/App.js
@@ -7,13 +7,17 @@ import ProfilePage from './pages/ProfilePage';
 import SignupPage from './pages/SignupPage';
 import EditProfilePage from './pages/EditProfilePage';
 import UploadPostPage from './pages/UploadPostPage';
+import WelcomePage from './pages/WelcomePage';
+import LoginPage from './pages/LoginPage'; 
+
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Navigate to="/signup" />} />
+        <Route path="/" element={<WelcomePage />} />
         <Route path="/signup" element={<SignupPage />} />
+        <Route path="/login" element={<LoginPage />} />
         <Route path="/timeline" element={<TimelinePage />} />
         <Route path="/edit-profile" element={<EditProfilePage />} />
         <Route path="/search" element={<SearchPage />} />
